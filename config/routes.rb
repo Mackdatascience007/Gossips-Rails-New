5970e346fdc23b887fd7b3521d8ca1f46670d93b
mackdatascience007 <mackdatascience@gmail.com>
2019-10-28 20:09:19 +0100

    First commit at all

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/team', to: 'pages#team'
+  get '/contact', to: 'pages#contact'
+  get '/home', to: 'pages#home'
+  get '/welcome/:first_name', to: 'pages#welcome'
+end
